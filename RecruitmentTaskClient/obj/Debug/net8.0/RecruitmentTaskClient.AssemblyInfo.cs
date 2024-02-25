4795c2ed8a8d4b9f9f20c7deb4f93f6d353c65b3
Michał Janaszek <michal.janaszek7@gmail.com>
2024-02-25 19:29:37 +0100

    Fixed sorting error

--- RecruitmentTaskClient/obj/Debug/net8.0/RecruitmentTaskClient.AssemblyInfo.cs
+++ RecruitmentTaskClient/obj/Debug/net8.0/RecruitmentTaskClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentTaskClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493648138511fc9fa23d31338439c7bdca41752e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e52a4a0e892d096a7198ef18f82221b37fe12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentTaskClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentTaskClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RecruitmentTaskShared/obj/Debug/net8.0/RecruitmentTaskShared.AssemblyInfo.cs
+++ RecruitmentTaskShared/obj/Debug/net8.0/RecruitmentTaskShared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentTaskShared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493648138511fc9fa23d31338439c7bdca41752e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e52a4a0e892d096a7198ef18f82221b37fe12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentTaskShared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentTaskShared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
